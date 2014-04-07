897c9eb8b0549fe6857fe6f6fdee0ad6f24752bb
Alex Palcuie <alex.palcuie@ubervu.com>
2014-04-07 23:39:24 +0300

    Implement sum

--- main.cpp
+++ main.cpp
@@ -25,18 +25,24 @@ THE SOFTWARE.
 #include <string>
 #include <cassert>
 #include <algorithm>
+#include <cstdio>
 #include <vector>
 #include <stdexcept>
 
 using std::string;
 using std::to_string;
 using std::reverse;
+using std::vector;
 
 class BigNumber {
   public:
+    vector<char> integerPart, fractionaryPart;
+    bool sign = 0;
+
     explicit BigNumber(int x) : BigNumber(to_string(x)) { }
     BigNumber(int integerPart, int fractionaryPart) :
         BigNumber(to_string(integerPart) + "." + to_string(fractionaryPart)) { }
+
     explicit BigNumber(string v) {
         int i = 0;
         if (v[i] == '-') {
@@ -67,6 +73,7 @@ class BigNumber {
         if (fractionaryPart.size())
             reverse(fractionaryPart.begin(), fractionaryPart.end());
     }
+
     string toString() {
         string answer;
         if (sign)
@@ -83,9 +90,63 @@ class BigNumber {
         return answer;
     }
 
+    void plus_one() {
+        int i = 0;
+        while (integerPart[i] == '9' && integerPart.size() > i) {
+            // printf("i = %d\n", i);
+            integerPart[i] = '0';
+            i++;
+        }
+        if (integerPart.size() == i)
+            integerPart.push_back('1');
+        else
+            integerPart[i]++;
+    }
+
+    void add(BigNumber x) {
+        reverse(x.fractionaryPart.begin(), x.fractionaryPart.end());
+
+        bool carry = 0;
+        std::tie(carry, fractionaryPart) = addTwoVectors(fractionaryPart,
+                                                         x.fractionaryPart);
+        if (carry)
+            plus_one();
+        std::tie(carry, integerPart) = addTwoVectors(integerPart,
+                                                     x.integerPart);
+        if (carry)
+            integerPart.push_back('1');
+    }
+
   private:
-    std::vector<char> integerPart, fractionaryPart;
-    bool sign = 0;
+    const int BASE = 10;
+
+    std::tuple<bool, vector<char>> addTwoVectors(vector<char> base,
+                                                 vector<char> visitor) {
+        // TODO(alex): change these reverses
+        reverse(base.begin(), base.end());
+        reverse(visitor.begin(), visitor.end());
+
+        while (base.size() < visitor.size())
+            base.push_back('0');
+        while (visitor.size() < base.size())
+            visitor.push_back('0');
+
+        reverse(base.begin(), base.end());
+        reverse(visitor.begin(), visitor.end());
+
+        bool carry = 0;
+        for (int i = 0; i < base.size(); i++) {
+            base[i] += visitor[i] + carry - '0';
+            if (base[i] > '9') {
+                carry = 1;
+                base[i] -= '9';
+            } else {
+                carry = 0;
+            }
+        }
+
+        return make_tuple(carry, base);
+    }
 };
 
 int main() {
@@ -93,10 +154,28 @@ int main() {
     assert(BigNumber(3).toString() == "3");
     assert(BigNumber(-5).toString() == "-5");
 
-    std::vector<string> v = {"10", "10.3", "-10.555", "69.34433434", "0", "2"};
+    vector<string> v = {"10", "10.3", "-10.555", "69.34433434", "0", "2"};
 
     for (int i = 0; i < v.size(); i++)
         assert(BigNumber(v[i]).toString() == v[i]);
+
+
+    BigNumber nineteen = BigNumber(19);
+    nineteen.plus_one();
+    assert(nineteen.toString() == "20");
+
+    BigNumber x = BigNumber(8);
+    x.plus_one();
+    assert(x.toString() == "9");
+    x.plus_one();
+    assert(x.toString() == "10");
+    BigNumber y = BigNumber("10.6");
+    y.plus_one();
+    assert(y.toString() == "11.6");
+
+    BigNumber ten = BigNumber("5.1");
+    ten.add(BigNumber("3.2"));
+    assert(ten.toString() == "8.3");
 
     return 0;
 }
